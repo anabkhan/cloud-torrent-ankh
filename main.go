15586fbf4d4954c87604ce4705be82e6d526e620
Anab Khan <akanabkhan@gmail.com>
2020-12-16 08:06:53 +0000

    changed import path

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"log"
 
-	"github.com/jpillora/cloud-torrent/server"
-	"github.com/jpillora/opts"
+	"./server"
+	//"github.com/jpillora/opts"
 )
 
 var VERSION = "0.0.0-src" //set with ldflags
@@ -16,11 +16,11 @@ func main() {
 		ConfigPath: "cloud-torrent.json",
 	}
 
-	o := opts.New(&s)
-	o.Version(VERSION)
-	o.PkgRepo()
-	o.LineWidth = 96
-	o.Parse()
+	// o := opts.New(&s)
+	// o.Version(VERSION)
+	// o.PkgRepo()
+	// o.LineWidth = 96
+	// o.Parse()
 
 	if err := s.Run(VERSION); err != nil {
 		log.Fatal(err)
